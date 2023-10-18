7342aaba5f8d6d9a1e58fd89d727900b8848c692
devrajAngu <devrajjena043@gmail.com>
2023-10-18 11:37:46 +0530

    newCreate

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, HostBinding, HostListener } from '@angular/core';
+import {  ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  name = 'Angular';
+
+  constructor( private router: Router , private route: ActivatedRoute){}
+
+
+
+
+  about(){
+    this.router.navigate(['/about'],{relativeTo:this.route})
+  }
+  loginAdmin(){
+    this.router.navigate(['/login'],{relativeTo:this.route})
+  }
+
+}
